6e0904dedb730e8441d40f1e48cb67bdfb120494
jagerluck <jagerluck@gmail.com>
2020-10-02 16:44:47 +0300

    tasks, gitignore

--- src/components/Todo/TaskPopup.js
+++ src/components/Todo/TaskPopup.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 
-const mock = new Date();
+const mock = new Date(2020, 9, 1);
+const mock = new Date(2020, 8, 30);
 
 export const TaskPopup = (props) => {
    const { taskText, taskDate, taskColor } = props;
